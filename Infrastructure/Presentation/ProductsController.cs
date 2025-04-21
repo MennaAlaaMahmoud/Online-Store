7bcabd073471158f5bbb43359b3f2165c17f1022
Admin <Admin@DESKTOP-1R0450K>
2025-04-21 17:31:40 +0400

    ProductController

--- Infrastructure/Presentation/ProductsController.cs
+++ Infrastructure/Presentation/ProductsController.cs
@@ -4,22 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ServicesAbstractions;
 
 namespace Presentation
 {
-    //// Api Controller
-    //[Route("api/[controller]")]
-    //public class ProductsController (IServiceManager) : ControllerBase
-    //{
-    //    // endpoint  : public non-static method
+    // Api
+    [ApiController]
+     [Route("api/[controller]")]
+    public class ProductsController (IServiceManager serviceManager) : ControllerBase
+    {
 
-    //    [HttpGet]
-    //    public Task< IActionResult> GetAllProducts()
-    //    {
+        [HttpGet] // endpoint : Get: /api/products
+        public async Task<IActionResult> GetAllProducts()
+        {
+           var result = await serviceManager.ProductService.GetAllProductsAsync();
+            if (result is null) return BadRequest(); // 400
+            return Ok(result); // 200                                       
 
+        }
 
-    //    }
+        [HttpGet("{id}")] // Get /api/products/{id}
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var result = await serviceManager.ProductService.GetProductByIdAsync(id);
+            if (result is null) return NotFound(); // 404
+            return Ok(result); // 200
+        }
 
+        // TODO : Get All Brands
+        // TODO : Get All Types
 
-    //}
+    }
 }
