876f4fbcc923948bbb229b43bd258205003f99f2
Admin <Admin@DESKTOP-1R0450K>
2025-04-22 22:52:05 +0400

    Sorting

--- Core/Services/Specificeations/BaseSpecifications.cs
+++ Core/Services/Specificeations/BaseSpecifications.cs
@@ -14,6 +14,8 @@ namespace Services.Specificeations
     {
         public Expression<Func<TEntity, bool>>? Criteria { get ; set ; }
         public List<Expression<Func<TEntity, object>>> IncludeExpressions { get ; set ; } = new List<Expression<Func<TEntity, object>>>();
+        public Expression<Func<TEntity, object>>? OrderBy { get; set ; }
+        public Expression<Func<TEntity, object>>? OrderByDescending { get ; set ; }
 
         public BaseSpecifications(Expression<Func<TEntity, bool>>? expression )
         {
@@ -26,5 +28,14 @@ namespace Services.Specificeations
             IncludeExpressions.Add(expression);
         }
 
+
+        protected void AddOrderBy(Expression<Func<TEntity, object>> expression)
+        {
+            OrderBy = expression;
+        }
+        protected void AddOrderByDescending(Expression<Func<TEntity, object>> expression)
+        {
+            OrderByDescending = expression;
+        }
     }
 }
