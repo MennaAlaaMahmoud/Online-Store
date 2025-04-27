b261e9175f4d57332cfaae9324ca8c65a2857e68
Admin <Admin@DESKTOP-1R0450K>
2025-04-28 00:27:43 +0400

    Refactoring Main Functions And Middleware

---
+++ Store.Menna.API/Extensions/Extensions.cs
@@ -0,0 +1,131 @@
+﻿using Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ErrorModels;
+using Store.Menna.API.Middlewares;
+
+namespace Store.Menna.API.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+
+            services.AddBuiltInServices();
+            services.AddSwaggerServices();
+
+            services.ConfigureServices();
+
+
+            return services;
+
+        }
+
+
+        private static IServiceCollection AddBuiltInServices(this IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+
+            return services;
+
+        }
+
+
+        private static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+
+
+            return services;
+
+        }
+
+
+        private static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+
+            services.Configure<ApiBehaviorOptions>(config =>
+            {
+                config.InvalidModelStateResponseFactory = (actionContext) =>
+                {
+                    var errors = actionContext.ModelState.Where(m => m.Value.Errors.Any())
+                                              .Select(m => new ValidationError()
+                                              {
+                                                  Field = m.Key,
+                                                  Errors = m.Value.Errors.Select(errors => errors.ErrorMessage)
+                                              });
+
+                    var response = new ValidationErrorResponse()
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(response);
+
+                };
+            });
+
+
+            return services;
+
+        }
+
+
+
+        public static async Task<WebApplication> ConfigureMiddleware(this WebApplication app)
+        {
+
+            await app.InitializeDatabaseAsync();
+            
+            app.UseGlobalErrorHandling();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            return app;
+        }
+
+
+
+        private static async Task<WebApplication> InitializeDatabaseAsync(this WebApplication app)
+        {
+            #region Seeding
+
+            using var scope = app.Services.CreateScope();
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>(); // ASK CLR Create Object From DbInitializer
+            await dbInitializer.InitializAsync();
+            #endregion
+
+
+            return app;
+        }
+
+
+        private static WebApplication UseGlobalErrorHandling(this WebApplication app)
+        {
+            app.UseMiddleware<GlobalErrorHandlingMiddleware>();
+
+            return app;
+
+        }
+
+    }
+}
--- Store.Menna.API/Middlewares/GlobalErrorHandlingMiddleware.cs
+++ Store.Menna.API/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -21,13 +21,7 @@ namespace Store.Menna.API.Middlewares
                 await _next.Invoke(context);
                 if(context.Response.StatusCode == StatusCodes.Status404NotFound)
                 {
-                    context.Response.ContentType = "application/json";
-                    var response = new ErrorDetails()
-                    {
-                        StatusCode = StatusCodes.Status404NotFound,
-                        ErrorMessage = $"End Point {context.Request.Path} is Not Found"
-                    };
-                    await context.Response.WriteAsJsonAsync(response);
+                    await HandlingNotFoundEndPointAsync(context);
                 }
             }
             catch (Exception ex)
@@ -35,35 +29,46 @@ namespace Store.Menna.API.Middlewares
                 // Log the exception
 
                 _logger.LogError(ex, ex.Message);
-
-                // 1. Set the response status code
-                // 2. Set the response content type
-                // 3. Set the response Object (body)
-                // 4. Return the response
-                //context.Response.StatusCode =StatusCodes.Status500InternalServerError;
-                context.Response.ContentType = "application/json";
-
-                var response = new ErrorDetails()
-                {
-                    ErrorMessage = ex.Message
-                };
-                response.StatusCode = ex switch
-                {
-                    NotFoundException => StatusCodes.Status404NotFound,
-                    _ => StatusCodes.Status500InternalServerError
-                };
-                context.Response.StatusCode = response.StatusCode;
-
-
-
-                await context.Response.WriteAsJsonAsync(response);
+                await HandlingErrorAsync(context, ex);
 
             }
-
-
-
         }
 
+        private static async Task HandlingErrorAsync(HttpContext context, Exception ex)
+        {
 
+            // 1. Set the response status code
+            // 2. Set the response content type
+            // 3. Set the response Object (body)
+            // 4. Return the response
+            //context.Response.StatusCode =StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = new ErrorDetails()
+            {
+                ErrorMessage = ex.Message
+            };
+            response.StatusCode = ex switch
+            {
+                NotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+            context.Response.StatusCode = response.StatusCode;
+
+
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+
+        private static async Task HandlingNotFoundEndPointAsync(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            var response = new ErrorDetails()
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                ErrorMessage = $"End Point {context.Request.Path} is Not Found"
+            };
+            await context.Response.WriteAsJsonAsync(response);
+        }
     }
 }
